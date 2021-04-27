5f418779a1d3e926f6eef1a0d1b7dfb964f9b3a7
Andrzej Knop <aknop07@gmail.com>
2021-04-27 17:33:49 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = amountElement.value;
+    let currency = currencyElement.value;
+    resultElement.innerText = amount * currency;
+})
