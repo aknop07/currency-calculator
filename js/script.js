32cb1bb627c8098de2c055dd77335e1e5f181a89
Andrzej Knop <aknop07@gmail.com>
2021-05-04 09:35:06 +0200

    Added OpenGraph and icon

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const calculateResult = (amount, currency) => {
+        const rateEUR = 4.56;
+        const rateUSD = 3.77;
+        const rateGBP = 5.24;
+        
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+            case "USD":
+                return amount / rateUSD;
 
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
-    resultElement.innerText = amount * currency;
-})
+            case "GBP":
+                return amount / rateGBP;
+        }
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
+        let amount = amountElement.value;
+        let currency = currencyElement.value;
+        let result = calculateResult(amount, currency);
+        
+        resultElement.innerHTML = `${result.toFixed(2)} ${currency}`;
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
+}
